e70abcf56a2b54a8157daa6cc4b158a986dcb360
Roman Pospíšilík <roman.pospisilik@flowmon.com>
2022-02-11 16:24:51 +0100

    More info displayed in adminTab  - order can be displayed by ID  - missing manual cancel action  - missing pagination for table

--- favi_extra_s2s/AdminFaviExtra.php
+++ favi_extra_s2s/AdminFaviExtra.php
@@ -2,6 +2,13 @@
 
 class AdminFaviExtra extends AdminTabCore {
 
+    const CREATE_ORDER_PATTERN = '~^\/tracking\/[a-z0-9]+\/orders$~';
+    const CANCEL_ORDER_PATTERN = '~^\/tracking\/[a-z0-9]+\/cancel-order$~';
+
+    const TYPE_CREATE_ORDER = 'create';
+    const TYPE_CANCEL_ORDER = 'cancel';
+    const TYPE_UNKNOWN = 'unknown';
+
     public function display() {
         $helper = new HelperForm();
         $helper->bootstrap = true;
@@ -20,18 +27,20 @@ class AdminFaviExtra extends AdminTabCore {
             'languages' => $this->context->controller->getLanguages(),
             'id_language' => $this->context->language->id,
         ];
-        //$this->addJqueryUI('ui.datepicker');
 
         echo $helper->generateForm([$this->getForm()]);
 
         $helperList = new HelperList();
-        $helperList->token = isset($_GET['token']) ? $_GET['token'] : Tools::getAdminTokenLite('AdminModules');
+        $helperList->token = isset($_GET['token']) ? $_GET['token'] : Tools::getAdminTokenLite('AdminFaviExtra');
         $helperList->identifier = 'id_favi';
         $helperList->show_toolbar = false;
         $helperList->bootstrap = true;
-        $helperList->table = 'orders';
+        $helperList->table = 'FaviOrders';
         $helperList->simple_header = true;
         $helperList->currentIndex = AdminController::$currentIndex . '';
+//        $helperList->actions = [
+//            'cancel',
+//        ];
         echo $helperList->generateList($this->getOrderList(), $this->getFieldList());
 
         echo '<script>jQuery("#content").removeClass("nobootstrap").addClass("bootstrap")</script>'; // because i don't know how say presta for using bootstrap
@@ -40,6 +49,7 @@ class AdminFaviExtra extends AdminTabCore {
     private function getFormValues() {
         $from = Tools::getValue('export_from');
         $to = Tools::getValue('export_to');
+        $orderId = Tools::getValue('order_id');
 
         if (!$from) {
             $from = date("Y-m-d", strtotime('-7 day'));
@@ -51,6 +61,7 @@ class AdminFaviExtra extends AdminTabCore {
         return [
             'export_from' => $from,
             'export_to' => $to,
+            'order_id' => $orderId,
         ];
     }
 
@@ -67,14 +78,22 @@ class AdminFaviExtra extends AdminTabCore {
                         'label' => $this->l('From'),
                         'name' => 'export_from',
                         'size' => 10,
-                        'required' => true,
+                        'required' => false,
                     ],
                     [
                         'type' => 'date',
                         'label' => $this->l('To'),
                         'name' => 'export_to',
                         'size' => 10,
-                        'required' => true,
+                        'required' => false,
+                    ],
+                    [
+                        'col' => 3,
+                        'type' => 'text',
+                        'label' => $this->l('Order'),
+                        'name' => 'order_id',
+                        'size' => 10,
+                        'required' => false,
                     ],
                 ],
                 'submit' => [
@@ -88,6 +107,7 @@ class AdminFaviExtra extends AdminTabCore {
         $orderList = [];
         $from = Tools::getValue('export_from');
         $to = Tools::getValue('export_to');
+        $orderId = (int)trim(Tools::getValue('order_id'));
 
         if (!$from) {
             $from = date("Y-m-d", strtotime('-7 day'));
@@ -96,7 +116,11 @@ class AdminFaviExtra extends AdminTabCore {
             $to = date("Y-m-d", strtotime('+1 day'));
         }
 
-        $sql = "SELECT * FROM `" . _DB_PREFIX_ . "favi_extra_s2s` WHERE `date` >= '" . $from . "' && `date` <= '" . $to . "' ORDER BY `id_favi` DESC;";
+        if ($orderId) {
+            $sql = "SELECT * FROM `" . _DB_PREFIX_ . "favi_extra_s2s` WHERE `id_order` = '" . $orderId . "' ORDER BY `id_favi` DESC;";
+        } else {
+            $sql = "SELECT * FROM `" . _DB_PREFIX_ . "favi_extra_s2s` WHERE `date` >= '" . $from . "' && `date` <= '" . $to . "' ORDER BY `id_favi` DESC;";
+        }
         try {
             $orders = Db::getInstance()->ExecuteS($sql, true, false);
         } catch (PrestaShopDatabaseException $e) {
@@ -104,9 +128,15 @@ class AdminFaviExtra extends AdminTabCore {
         }
         if ($orders) {
             foreach ($orders as $order) {
+                $data = json_decode($order['data']);
                 $orderList[] = [
                     'id_favi' => $order['id_favi'],
-                    'data' => $order['data'],
+                    'id_order' => $order['id_order'],
+                    'date' => $order['date'],
+                    'httpCode' => $data->httpCode,
+                    'response' => json_encode($data->response),
+                    'customer' => $data->request->customer->name . "\n<" . $data->request->customer->email . '>',
+                    'type' => $this->getCallType($data->url),
                 ];
             }
         }
@@ -121,10 +151,43 @@ class AdminFaviExtra extends AdminTabCore {
                 'remove_onclick' => true,
                 'class' => 'fixed-width-xs',
             ],
-            'data' => [
-                'title' => $this->l('Data'),
+            'id_order' => [
+                'title' => $this->l('Order'),
+                'remove_onclick' => true,
+                'class' => 'fixed-width-xs',
+            ],
+            'date' => [
+                'title' => $this->l('Date'),
+                'remove_onclick' => true,
+                'class' => 'fixed-width-xl',
+            ],
+            'customer' => [
+                'title' => $this->l('Customer'),
+                'remove_onclick' => true,
+            ],
+            'httpCode' => [
+                'title' => $this->l('HTTP code'),
+                'remove_onclick' => true,
+                'class' => 'fixed-width-xs',
+            ],
+            'response' => [
+                'title' => $this->l('FAVI response'),
+                'remove_onclick' => true,
+            ],
+            'type' => [
+                'title' => $this->l('Type'),
                 'remove_onclick' => true,
             ],
         ];
+    }
+
+    private function getCallType($url) {
+        if (preg_match(self::CREATE_ORDER_PATTERN, $url)) {
+            return self::TYPE_CREATE_ORDER;
+        } elseif (preg_match(self::CANCEL_ORDER_PATTERN, $url)) {
+            return self::TYPE_CANCEL_ORDER;
+        }
+
+        return self::TYPE_UNKNOWN;
     }
 }
